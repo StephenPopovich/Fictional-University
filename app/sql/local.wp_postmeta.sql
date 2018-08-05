6a49f3426948deaa451c039eb939420ac9e47b85
StephenPopovich <stephenpopovich16@gmail.com>
2018-08-05 13:09:07 -0700

    Add about page and privacy policy page. Update php title rendering and link rendering

---
+++ app/sql/local.wp_postmeta.sql
@@ -0,0 +1,12 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_postmeta` VALUES
+(1,2,"_wp_page_template","default"),
+(2,3,"_wp_page_template","default"),
+(12,9,"_edit_last","1"),
+(13,9,"_edit_lock","1532806531:1"),
+(16,11,"_edit_last","1"),
+(17,11,"_edit_lock","1532810593:1"),
+(22,13,"_edit_last","1"),
+(23,13,"_edit_lock","1532810749:1");
---
+++ app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"Stephen","$P$Bh.JmFSjPZ8eTmJa1rLkW./zRfEwI6/","stephen","stephenpopovich@gmail.com","","2018-07-25 22:51:01","",0,"Stephen");
